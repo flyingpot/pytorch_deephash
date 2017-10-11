4ab14d1199df01659de017666f8722750f4a8a66
fjb <fjb@pku.edu.cn>
2017-10-11 22:13:48 +0800

    required_grad = False

--- main.py
+++ main.py
@@ -2,19 +2,32 @@ import torch
 import torch.nn as nn
 
 from torchvision import datasets, models
+from torch.autograd import Variable
 
-trainset = datasets.CIFAR10(root='./data', train=True, download=True)
-trainloader = torch.utils.data.Dataloader(trainset, batch_size=128,
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+trainset = datasets.CIFAR10(root='./data', train=True, download=True,
+                            transform=transform_train)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=128,
                                           shuffle=True, num_workers=2)
 
-testset = datasets.CIFAR10(root='./data', train=True, download=True)
+testset = datasets.CIFAR10(root='./data', train=True, download=True,
+                           transform=transform_test)
 testloader = torch.utils.data.Dataloader(testset, batch_size=100,
                                          shuffle=True, num_workers=2)
 
 alexnet_model = models.alexnet(pretrained=True)
 
-for param in alexnet_model.parameters():
-    param.requires_grad = False
+#for param in alexnet_model.parameters():
+#    param.requires_grad = False
 
 alexnet_model.classifier._modules['6'] = nn.Linear(4096, 48)
 alexnet_model.classifier._modules['7'] = nn.Sigmoid()
@@ -34,6 +47,7 @@ def train(epoch):
         optimizer.zero_grad()
         output = model(data)
         loss = criterion(output, target)
+        loss.backward()
         optimizer.step()
         if batch_idx % 10 == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.
