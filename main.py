1201bb267147ff72f481c0575a27e04f2b8a3902
flyingpot <fanjingbo1@gmail.com>
2017-10-12 09:44:13 +0800

    train complete

--- main.py
+++ main.py
@@ -1,33 +1,37 @@
 import torch
 import torch.nn as nn
 
-from torchvision import datasets, models
+from torchvision import datasets, models, transforms
 from torch.autograd import Variable
 
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+#transform_train = transforms.Compose([
+#    transforms.RandomCrop(32, padding=4),
+#    transforms.RandomHorizontalFlip(),
+#    transforms.ToTensor(),
+#    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+#])
+#transform_test = transforms.Compose([
+#    transforms.ToTensor(),
+#    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+#])
+transform = transforms.Compose(
+    [transforms.CenterCrop(227),
+     transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 trainset = datasets.CIFAR10(root='./data', train=True, download=True,
-                            transform=transform_train)
+                            transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=128,
                                           shuffle=True, num_workers=2)
 
 testset = datasets.CIFAR10(root='./data', train=True, download=True,
-                           transform=transform_test)
-testloader = torch.utils.data.Dataloader(testset, batch_size=100,
+                           transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=100,
                                          shuffle=True, num_workers=2)
 
 alexnet_model = models.alexnet(pretrained=True)
 
-#for param in alexnet_model.parameters():
-#    param.requires_grad = False
+for param in alexnet_model.parameters():
+    param.requires_grad = False
 
 alexnet_model.classifier._modules['6'] = nn.Linear(4096, 48)
 alexnet_model.classifier._modules['7'] = nn.Sigmoid()
@@ -38,7 +42,8 @@ model.cuda()
 
 criterion = nn.CrossEntropyLoss()
 
-optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+params = list(model.classifier._modules['6'].parameters()) + list(model.classifier._modules['7'].parameters()) + list(model.classifier._modules['8'].parameters())  
+optimizer = torch.optim.SGD(params, lr=0.01, momentum=0.5)
 
 def train(epoch):
     model.train()
